52701b03a4ed2edd56e9b06fe0c57bb915992f93
cocorogi <cocorogi@hotmail.co.jp>
2026-05-23 20:28:25 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,35 +1,10 @@
-const CACHE_NAME = 'bodylog-v3';
-const urlsToCache = [
-  '/cocobody/diet_app_v2.html',
-  '/cocobody/manifest.json'
-];
-
-// 古いキャッシュを全て削除
+self.addEventListener('install', () => self.skipWaiting());
 self.addEventListener('activate', event => {
   event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return Promise.all(
-        cacheNames.map(cacheName => {
-          if(cacheName !== CACHE_NAME) {
-            return caches.delete(cacheName);
-          }
-        })
-      );
-    }).then(() => self.clients.claim())
+    caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k))))
+    .then(() => self.clients.claim())
   );
 });
-
-self.addEventListener('install', event => {
-  self.skipWaiting();
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
-  );
-});
-
 self.addEventListener('fetch', event => {
-  event.respondWith(
-    fetch(event.request).catch(() =>
-      caches.match(event.request)
-    )
-  );
+  event.respondWith(fetch(event.request));
 });
